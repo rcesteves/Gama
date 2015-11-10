3878ea7517738489f0df4c2fec5c3a400ad6cc86
elielsonbs <fgjhfgjgjgj>
2015-11-10 21:16:42 -0200

    Criação das classes Plano e Convênio.

---
+++ SysPark/WindowsFormsApplication1/Plano.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Plano
+    {
+        private int CodPlano;
+        private string DescPlano;
+        private int QuantidadeDias;
+        private float ValorDia;
+        private string ObservacaoPlano;
+
+        public int CodPlano1
+        {
+            get
+            {
+                return CodPlano;
+            }
+
+            set
+            {
+                CodPlano = value;
+            }
+        }
+
+        public string DescPlano1
+        {
+            get
+            {
+                return DescPlano;
+            }
+
+            set
+            {
+                DescPlano = value;
+            }
+        }
+
+        public int QuantidadeDias1
+        {
+            get
+            {
+                return QuantidadeDias;
+            }
+
+            set
+            {
+                QuantidadeDias = value;
+            }
+        }
+
+        public float ValorDia1
+        {
+            get
+            {
+                return ValorDia;
+            }
+
+            set
+            {
+                ValorDia = value;
+            }
+        }
+
+        public string ObservacaoPlano1
+        {
+            get
+            {
+                return ObservacaoPlano;
+            }
+
+            set
+            {
+                ObservacaoPlano = value;
+            }
+        }
+    }
+}
